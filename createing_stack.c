e36599c6dc7c40a71352c6a327b101ec0d898a94
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-08-04 01:29:20 +0530

    Create createing_stack.c

---
+++ createing_stack.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct Stack
+{
+    int arr[5];
+    int tos;
+};
+
+int main()
+{
+    struct Stack S;
+    S.tos = -1;
+    S.tos = S.tos + 1;
+    S.arr[S.tos] = 10;
+    S.tos = S.tos + 1;
+    S.arr[S.tos] = 20;
+    S.tos = S.tos + 1;
+    S.arr[S.tos] = 30;
+    
+    printf("Element at tos:"); 
+    for(int i=0;i<S.tos;i++)
+    {
+      printf("%d",S.arr[i]);
+    }
+   printf("\n");
+    return 0;
+}
+
+//OUTPUT WOULD BE - 
+//Elements in the stack: 10 20 30 
